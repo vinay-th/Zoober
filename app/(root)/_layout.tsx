90f1c9eacd65bc4ef2f2aae6b83a93ec8748b92d
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-11 17:07:18 +0530

    ui fixed in confirm-ride

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="find-ride" options={{ headerShown: false }} />
       <Stack.Screen name="confirm-ride" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="book-ride" options={{ headerShown: false }} /> */}
+      <Stack.Screen name="book-ride" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -18,7 +18,7 @@ const ConfirmRide = () => {
           <DriverCard
             item={item}
             selected={selectedDriver!}
-            setSelected={() => setSelectedDriver(item.id!)}
+            setSelected={() => setSelectedDriver(Number(item.id)!)}
           />
         )}
         ListFooterComponent={() => (
